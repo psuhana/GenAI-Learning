a6d98efa1f0ebe9a4a1284f5596e2cbd25fd4cd2
suhanap <63767271+psuhana@users.noreply.github.com>
2026-06-06 20:39:17 +0530

    Add files via upload

--- 10_fastapi/fastapi_01_intro.py
+++ 10_fastapi/fastapi_01_intro.py
@@ -1,1 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
 
+class QuestionRequest(BaseModel):
+    question : str
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {
+        "message":"Hello AI Systems"
+    }
+
+@app.get('/about')
+def about():
+    return {
+        "name":"GenAI Learning API",
+        "version":"1.0",
+        "student":"suhana",
+        "reporting to": "sensei-gpt"
+    }
+
+@app.post('/ask')
+def ask(data:QuestionRequest):
+    return {
+        "recieved_question": data.question
+    }
