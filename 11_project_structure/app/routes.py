470fd96d00f0c28ff74993818a85ffae698fc39b
suhanap <63767271+psuhana@users.noreply.github.com>
2026-06-08 22:24:59 +0530

    Add files via upload

--- 11_project_structure/app/routes.py
+++ 11_project_structure/app/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from models import QuestionRequest
 from rag import rag_pipeline
 
@@ -6,4 +6,16 @@ router = APIRouter()
 
 @router.post("/ask")
 def ask(data: QuestionRequest):
+    if data.question == "":
+        raise HTTPException(
+            status_code= 400,
+            detail= "Question cannot be empty!"
+        )
     return rag_pipeline(data.question)
+
+@router.get('/health')
+def health():
+    return{
+        "status": "healthy"
+    }
+
